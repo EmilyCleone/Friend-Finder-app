96b1eb570122c7e51ea52ca2ef1df0b94a9b10cb
Emily Johnson-Eaton <emilyjohnsoneaton@gmail.com>
2019-10-05 22:27:55 -0600

    added port to connect to server on server.js page.

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -2,3 +2,18 @@
 var express = require("express");
 var path = require("path");
 
+//sets up express
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
